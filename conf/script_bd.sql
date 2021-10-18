0e0817e77309f98efcf7654cdbbe7f04ff44f012
Sara Machado <sarasanto1309@gmail.com>
2021-10-18 20:34:42 -0300

    TASK - 002: Alteração no arquivo script_bd.sql
    Foi adicionado o código de criação das tabelas.

--- conf/script_bd.sql
+++ conf/script_bd.sql
@@ -1,5 +1,42 @@
-CREATE TABLE `marca` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `atleta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `peso` float,
+  `altura` float,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `vendedor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45),
+  `senha` varchar(45),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `pessoa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `horaEntrada` time,
+  `horaSaida` time,
+  `idade` int,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `time` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `cidade` varchar(45),
+  `pontos` int,
+  `dataFundacao` date,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `peca` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
+  `valor` double,
+  `fornecedor` varchar(45),
+  `garantia` date,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
